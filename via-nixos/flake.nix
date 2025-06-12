e3515d36f692a4a8588d641e55a1b98ccd16d07b
Marseille Bouchard Demko <marseillebd@proton.me>
2025-06-12 09:33:34 -0400

    track my nixos configuration

---
+++ via-nixos/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "My first Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      lazarenko-nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
