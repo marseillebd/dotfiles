afd065f15941c3f61485aa0c8304a6a3d77b8b72
Marseille Bouchard Demko <marseillebd@proton.me>
2025-06-13 00:27:36 -0400

    steam and librewolf

--- via-nixos/configuration.nix
+++ via-nixos/configuration.nix
@@ -109,7 +109,16 @@
     stow
     tree
     obsidian
+    librewolf
+    steam
   ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
