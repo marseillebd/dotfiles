186484a4cb268e84d1eca0b242d6145e04555795
Marseille Bouchard Demko <marseillebd@proton.me>
2026-04-12 21:15:36 -0400

    update nix channel

--- via-nixos/flake.nix
+++ via-nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "My first Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs, ... }:
